5f7e8bdef12a647a741f59d6f4330b840cf6754c
rhutuja3010 <91016307+rhutuja3010@users.noreply.github.com>
2022-05-08 11:12:48 +0530

    Add files via upload

---
+++ Q.py
@@ -0,0 +1,89 @@
+# a=[2,4,1,8,9]
+# print(a[2])
+# print(len(a[8]))
+# i=0
+# while i<len(a):
+#     print("Index",i,"Element",a[i])
+#     i+=1
+
+# i=1
+# while i<=5:
+    # j=1
+    # while j<=i:
+    #     print(i**2,end="")
+    #     j+=1
+    # print()
+    # i+=1
+
+# string="kranti"
+# i=0
+# while i<6:
+#     b=1
+#     while b<6-i:
+#         b=b+1
+#         print("",end="")
+#     j=0
+#     while j<=i:
+#         print(string[j],end="")
+#         j=j+1
+#     print()
+#     i=i+1
+
+
+# def my(a):
+#     # a=[1,4,8,4]
+#     print(max(a))
+# my([1,4,8,4])
+
+# a=2+5j
+# print(type(a))
+
+# a=[2,6,-4,9,-3,5]
+# c1=0
+# c2=0
+# for i in range(len(a)):
+#     if (a[i])>0:
+#         c1+=1
+#     else:
+#         c2+=1
+# print(c1)
+# print(c2)
+
+# a=2+5j
+# b=3+2j
+# print(a*b)
+
+# a=[10,200,31,550]
+# for i in  range(len(a)):
+#     b=a[i]%10
+#     if b==0:
+#         print(a[i])
+#     # print(b)
+
+
+# a=["rhutuja","likhitha","Aman","monu"]
+# max=a[0]
+# for i in a:
+#     # print(i)
+#     if i>max:
+#         max=i
+# print(max)
+
+
+# a=4
+# print(complex(a))
+
+# s="Rhutuja"
+# print(type(s))
+
+# a=89
+# print(type(a))
+
+
+# a=3+2j
+# b=int(a)
+# print(b)
+
+
+# a=3
+# print(float(a))
